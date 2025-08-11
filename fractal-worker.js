93422c03cfb6906dbd4df5b2d95fdf7233925a04
robkralik <robkralik@gmail.com>
2025-08-10 21:45:50 -0700

    Delete fractal-worker.js

--- fractal-worker.js
+++
@@ -1,8 +0,0 @@
-// This is the simplest possible Web Worker.
-// It should immediately send a message back to the main thread.
-
-console.log("Worker: Script loaded and starting.");
-
-self.postMessage({ message: "SUCCESS: Web Worker is running!" });
-
-console.log("Worker: Message sent.");
